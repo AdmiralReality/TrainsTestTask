31425dd7f24c3253da1bc53c7c96838dd7e0b9e6
Paul Pestov <pavel.pestov@gmail.com>
2023-03-13 00:54:39 +0400

    Build prototype interface for CustomControl (currently on main window)

---
+++ TrainsTestTask/CustomControls/CanvasControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace TrainsTestTask.CustomControls
+{
+    public class CanvasControl : Control
+    {
+        public static readonly DependencyProperty ShapesProperty
+			= DependencyProperty.Register(nameof(Shapes), typeof(List<Shape>), typeof(CanvasControl),
+				new PropertyMetadata(() => new List<Shape>())); // TODO huh?
+
+
+		private List<Shape> shapes;
+		public List<Shape> Shapes
+		{
+			get { return (List<Shape>)GetValue(ShapesProperty); }
+			set { SetValue(ShapesProperty, value); }
+		}
+
+
+	}
+}
